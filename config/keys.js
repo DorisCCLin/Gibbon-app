25e240c4924f4a41968481420a2bb776959d9e53
DorisCCLin <doriscclin@gmail.com>
2018-03-25 22:48:46 -0400

    auth flow set

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 // keys.js - credential selection
-if (production.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
 	// we are in production - return the prod set of keys.
 	module.exports = require('./prod');
 } else {
