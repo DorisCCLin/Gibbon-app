4af7500b22e222b63b6a0e4d00ad3b1bab3304e2
DorisCCLin <doriscclin@gmail.com>
2018-03-30 14:11:02 -0400

    final

--- notes/async-promise.js
+++ notes/async-promise.js
@@ -16,3 +16,14 @@ const fetchAlbums = async () => {
 };
 
 fetchAlbums();
+
+// This is because the port you are using to run the script is already in use. You have to stop all other node which are using that post . for that you can check all node by
+
+// ps -e
+// to Kill all node process
+
+// sudo killall -9 node
+// killall node
+// "webhook": "./sendgrid_webhook.sh"
+// "dev":
+    //   "concurrently \"npm run server\" \"npm run client\" \"npm run webhook\"",
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,26 +10,57 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-	app.get('/api/surveys/thanks', (req, res) => {
+	app.get('/api/surveys', requireLogin, async (req, res) => {
+		const surveys = await Survey.find({ _user: req.user.id }).select({
+			recipients: false
+		});
+
+		res.send(surveys);
+	});
+
+	app.get('/api/surveys/:surveyId/:choice', (req, res) => {
 		res.send('thanks for voting');
 	});
 
 	app.post('/api/surveys/webhooks', (req, res) => {
 		// console.log(req.body);
 		// res.send({});
-		const events = _.map(req.body, ({ email, url }) => {
-			const pathname = new URL(url).pathname;
-			const p = new Path('/api/surveys/:surveyId/:choice');
-			const match = p.test(pathname);
-			if (match) {
-				return {
-					email,
-					surveyId: match.surveyId,
-					choice: match.choice
-				};
-			}
-		});
-		console.log(events);
+		const p = new Path('/api/surveys/:surveyId/:choice');
+
+		const test = _.chain(req.body)
+			.map(({ email, url }) => {
+				const match = p.test(new URL(url).pathname);
+				if (match) {
+					return {
+						email,
+						surveyId: match.surveyId,
+						choice: match.choice
+					};
+				}
+			})
+			.compact()
+			.uniqBy('email', 'surveyId')
+			.each(({ surveyId, email, choice }) => {
+				Survey.updateOne(
+					{
+						// have to add _ to keys as mongoDB rule, mongoose doensn't need.
+						_id: surveyId,
+						recipients: {
+							$elemMatch: { email: email, responded: false }
+						}
+					},
+					{
+						$inc: { [choice]: 1 },
+						$set: { 'recipients.$.responded': true },
+						lastResponded: new Date()
+					}
+				).exec();
+			})
+			.value();
+
+		console.log(test);
+
+		res.send({});
 	});
 
 	app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
